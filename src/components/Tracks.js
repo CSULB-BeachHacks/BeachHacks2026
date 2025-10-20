5ce08876c19d7c9e5c7e2fa4bfecacf09c66ddaf
Detims <2005.nnhan@gmail.com>
2025-10-19 21:53:10 -0700

    add backgruond images

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -8,6 +8,9 @@ import cyanClosed from './TrackImages/cyan_closed 1.svg';
 import cyanOpen from './TrackImages/cyan_open 1.svg';
 import navyClosed from './TrackImages/navy_closed 1.svg';
 import navyOpen from './TrackImages/navy_open 1.svg';
+import bluePurpleStar from "./TrackImages/blue_purple_star.svg";
+import purpleBlueStar from "./TrackImages/purple_blue_star.svg";
+import purpleSmallStar from "./TrackImages/purple_small_star.svg";
 
 const Tracks = () => {
   const tracks = [
@@ -44,10 +47,12 @@ const Tracks = () => {
   ];
 
   return (
-    <section className="w-full flex flex-col justify-center items-center" id="tracks">
+    <section className="relative w-full flex flex-col justify-center items-center" id="tracks">
       <h1 className="text-6xl pt-12 pb-24"> 
         Tracks
       </h1>
+
+      <img className="absolute" src={purpleBlueStar} alt="" />
 
       <div className="grid grid-cols-2 gap-8 gap-x-24 pb-12">
         {tracks.map((item, index) => {
