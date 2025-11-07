b881b8ffa54f404e21946079fb3e7a8eb371f986
Detims <2005.nnhan@gmail.com>
2025-11-06 21:30:18 -0800

    remove tailwind

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -2,7 +2,7 @@ import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import TrackListItem from "./TrackListItem";
-import '../index.css'
+import "./Tracks.css"; // ← import the CSS file
 
 import lightBlueClosed from './TrackImages/lightblue_closed 1.svg';
 import lightBlueOpen from './TrackImages/lightblue_open 1.svg';
@@ -16,63 +16,36 @@ import purpleSmallStar from "./TrackImages/purple_small_star.svg";
 
 const Tracks = () => {
   const tracks = [
-    {
-      open: lightBlueOpen,
-      closed: lightBlueClosed,
-      name: "Track 1",
-    },
-    {
-      open: navyOpen,
-      closed: navyClosed,
-      name: "Track 2",
-    },
-    {
-      open: cyanOpen,
-      closed: cyanClosed,
-      name: "Track 3",
-    },
-    {
-      open: lightBlueOpen,
-      closed: lightBlueClosed,
-      name: "Track 4",
-    },
-    {
-      open: navyOpen,
-      closed: navyClosed,
-      name: "Track 5",
-    },
-    {
-      open: cyanOpen,
-      closed: cyanClosed,
-      name: "Track 6",
-    },
+    { open: lightBlueOpen, closed: lightBlueClosed, name: "Track 1" },
+    { open: navyOpen,     closed: navyClosed,      name: "Track 2" },
+    { open: cyanOpen,     closed: cyanClosed,      name: "Track 3" },
+    { open: lightBlueOpen,closed: lightBlueClosed, name: "Track 4" },
+    { open: navyOpen,     closed: navyClosed,      name: "Track 5" },
+    { open: cyanOpen,     closed: cyanClosed,      name: "Track 6" },
   ];
 
   useGSAP(() => {
     gsap.to('.hover', {
-        y: -660,
-        duration: 5,
-        repeat: -1,
-        yoyo: true,
-        ease: 'power1.inOut',
+      y: -660,
+      duration: 5,
+      repeat: -1,
+      yoyo: true,
+      ease: 'power1.inOut',
     });
-    }, [])
+  }, []);
 
   return (
-    <section className="relative w-full flex flex-col justify-center items-center" id="tracks">
-      <h1 className="text-8xl pt-12 pb-24 font-bold"> 
-        Tracks
-      </h1>
+    <section className="tracks" id="tracks">
+      <h1 className="tracks__title">Tracks</h1>
 
-      <img className="absolute -translate-x-[40rem] -translate-y-[36rem] scale-[250%] rotate-[33deg]" src={purpleBlueStar} alt="" />
-      <img className="absolute translate-x-[40rem] -translate-y-[42rem] scale-75 rotate-[-17deg] hover" src={purpleSmallStar} alt="" />
-      <img className="absolute scale-[250%] translate-x-[30rem] translate-y-[26rem] rotate-[32deg]" src={bluePurpleStar} alt="" />
-      <div className="grid grid-cols-2 gap-8 gap-x-24 pb-12">
-        {tracks.map((item, index) => {
-          return (
-            <TrackListItem key={index} open={item.open} closed={item.closed} name={item.name} />
-          );
-        })}
+      <img className="star--top-left" src={purpleBlueStar} alt="" />
+      <img className="star--small hover" src={purpleSmallStar} alt="" />
+      <img className="star--bottom-right" src={bluePurpleStar} alt="" />
+
+      <div className="tracks__grid">
+        {tracks.map((item, index) => (
+          <TrackListItem key={index} open={item.open} closed={item.closed} name={item.name} />
+        ))}
       </div>
     </section>
   );
