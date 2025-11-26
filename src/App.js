349293f0b7f71b7ea4f76dacd2b315f69e6c9a75
VanishedAIR <vanshp.1005@gmail.com>
2025-11-25 16:59:49 -0800

    create dashboard, routing (AI), add correct labels instead of spamming div's

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 // Import all components
@@ -11,31 +12,48 @@ import FAQ from "./components/FAQ";
 import Sponsors from "./components/Sponsors";
 import Teams from "./components/Teams";
 import Application from "./components/Application";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 // Import Auth Provider
 import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-  return (
-    <AuthProvider>
-      {/* whole site has background now */}
-      <div className="App is-default">
-        {/* Hero keeps its own background */}
-        <div className="is-landing">
-          <Navbar />
-          <Hero />
-        </div>
-        {/* rest of the site sits on site-wide background */}
-        {/* <Application /> */}
-        <About />
-        <Tracks />
-        <Speakers />s
-        <FAQ />
-        <Sponsors />
-        <Teams />
-      </div>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <div className="App is-default">
+                                {/*Hero keeps its own background */}
+                                <div className="is-landing">
+                                    <Navbar />
+                                    <Hero />
+                                </div>
+                                {/* rest of the site sits on site-wide background */}
+                                {/* <Application /> */}
+                                <About />
+                                <Tracks />
+                                <Speakers />
+                                <FAQ />
+                                <Sponsors />
+                                <Teams />
+                            </div>
+                        }
+                    />
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <div className="App is-landing">
+                                <Dashboard />
+                            </div>
+                        }
+                    />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
