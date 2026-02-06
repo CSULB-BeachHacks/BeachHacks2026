966fe77978cca66b06bc2f6b7d79a64315c93473
VanishedAIR <vanshp.1005@gmail.com>
2026-02-05 20:42:48 -0800

    Add visual key to verify deployment

--- src/components/EmailSignup.js
+++ src/components/EmailSignup.js
@@ -132,7 +132,7 @@ export default function EmailSignup() {
                     ref={teaserRef}
                     className="email-signup__teaser email-signup__teaser--glow"
                 >
-                    Applications begin soon!
+                    Applications begin soon! [TEST]
                 </h2>
                 <p ref={subtitleRef} className="email-signup__subtitle">
                     Stay tuned for applications opening
